6b40ea73ed9ca90fedfa94a61b911f476529d749
Leonardo Lima <leonardolimarj@outlook.com>
2023-02-27 21:16:40 -0300

    seguindo
    Polimorfismo

--- modelo.py
+++ modelo.py
@@ -5,6 +5,9 @@ class Programa:
         self.ano = ano
         self._likes = 0
 
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
+
     @property
     def likes(self):
         return self._likes
@@ -19,16 +22,34 @@ class Programa:
     @nome.setter
     def nome(self, nome):
         self._nome = nome
+
+    def imprime(self):
+        print(f'{self._nome} - {self.ano}- {self._likes} likes')
+
 #classe filha herdando nome e ano e criando o atributo duração
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
-        
+
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
+
+    #sobrescrevendo o metodo imprime
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes')
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
+
+    #sobrescrevendo o metodo imprime
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} likes')
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -41,3 +62,9 @@ atlanta.dar_likes()
 
 print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
 print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+
+filmes_e_series= [vingadores, atlanta]
+
+#procura na lista filmes_e_series se dentro de cada objeto há o atributo 'duracao'
+for programa in filmes_e_series:
+    print(programa)
