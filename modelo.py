37f032a9a337a173319ab5f730b37a9b8c9cc84d
Leonardo Lima <leonardolimarj@outlook.com>
2023-03-01 15:33:17 -0300

    Seguindo
    Polimorfismo

--- modelo.py
+++ modelo.py
@@ -51,20 +51,41 @@ class Serie(Programa):
     def imprime(self):
         print(f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} likes')
 
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
+
+
+
+#criando objeto
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
+
+
+#criando objeto
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
-
+#'dando' likes p/ teste
 atlanta.dar_likes()
+vingadores.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+vingadores.dar_likes()
 atlanta.dar_likes()
+#criando lista
+filmes_e_series= [vingadores, atlanta, demolidor, tmep]
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+pl_fim_de_semana = Playlist('Fim de Semana', filmes_e_series)
 
-filmes_e_series= [vingadores, atlanta]
 
-#procura na lista filmes_e_series se dentro de cada objeto há o atributo 'duracao'
-for programa in filmes_e_series:
+
+print(f'Tamanho do Playlist: {len(pl_fim_de_semana)}')
+
+#procura na lista pl_fim_de_semana se dentro de cada objeto há o atributo 'duracao'
+for programa in pl_fim_de_semana:
     print(programa)
